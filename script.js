cbe08270bb02938e765e51627163f1aceb55e2b1
TestUserOnlineNL <therubyfan@outlook.com>
2022-09-28 14:03:31 +0200

    changed dutch text to english

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ const greetings = "hello world!";
 
 for (let r = 1; r <= 10; r++) {
   let n = r.toString().padStart(3, '0');
-  console.log(`regel ${n} ${greetings}`);
+  console.log(`line ${n} ${greetings}`);
 }

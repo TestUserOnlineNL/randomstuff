d09d6d637f499a27ae1c16aac1ee8c41c20d8bf3
TestUserOnlineNL <testuser.onlinenl@gmail.com>
2022-11-03 15:30:07 +0100

    restructured code and add filter class

--- filtering-by-shape-color-or-price/dist/script.js
+++ filtering-by-shape-color-or-price/dist/script.js
@@ -1,175 +1,301 @@
 //============================
 // function lprint
 const lprint = (data) => {
-    const element = document.createElement("section");
-    element.textContent = data;
-    return document.body.appendChild(element), console.log(data);
+  const element = document.createElement("section");
+  element.textContent = data;
+  return document.body.appendChild(element), console.log(data);
 };
 //============================
 // function lprintArr
 const lprintArr = (arrToPrint) => {
-    for (let i = 0; i < arrToPrint.length; i++) {
-        lprint(arrToPrint[i]);
-    }
+  for (let i = 0; i < arrToPrint.length; i++) {
+    lprint(arrToPrint[i]);
+  }
 };
 //============================
 // function dateTimestamp
 const dateTimestamp = () => {
-    options = {
-        year: "numeric",
-        month: "2-digit",
-        day: "2-digit",
-        hour: "numeric",
-        minute: "numeric",
-        second: "numeric",
-        //fractionalSecondDigits: 3,
-        hour12: false,
-        timeZone: "Europe/Amsterdam"
-    };
-    return new Intl.DateTimeFormat("nl-NL", options).format();
+  options = {
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+    hour: "numeric",
+    minute: "numeric",
+    second: "numeric",
+    //fractionalSecondDigits: 3,
+    hour12: false,
+    timeZone: "Europe/Amsterdam"
+  };
+  return new Intl.DateTimeFormat("nl-NL", options).format();
 };
 //============================
-// filters
+////////////////////////////////////////////////////
+/*
+  SHAPES OBJECTS
+
+  Classes: Shapes, FilterObjects, ShowObjects 
+*/
+////////////////////////////////////////////////////
+const showShapes = document.querySelector("#shapes");
+const myShapes = [];
+
+class Shapes {
+  constructor(shape, size) {
+    this.shape = shape;
+    this.size = size;
+    this.color = "white";
+    this.price = 0;
+  }
+  circle() {
+    const shape = document.createElement("div");
+    shape.style.width = `${this.size}px`;
+    shape.style.height = `${this.size}px`;
+    shape.style.backgroundColor = this.color;
+    shape.style.borderRadius = "50%";
+    shape.style.display = "flex";
+    shape.style.justifyContent = "space-around";
+    shape.style.alignItems = "center";
+    shape.innerText = `€${this.price}`;
+    shape.setAttribute("id", `${this.shape} ${this.color} ${this.price}`);
+    return shape;
+  }
+  square() {
+    const shape = document.createElement("div");
+    shape.style.width = `${this.size}px`;
+    shape.style.height = `${this.size}px`;
+    shape.style.backgroundColor = this.color;
+    shape.style.display = "flex";
+    shape.style.justifyContent = "space-around";
+    shape.style.alignItems = "center";
+    shape.innerText = `€${this.price}`;
+    shape.setAttribute("id", `${this.shape} ${this.color} ${this.price}`);
+    return shape;
+  }
+  triangle() {
+    const shape = document.createElement("div");
+    shape.style.width = `${0}px`;
+    shape.style.height = `${0}px`;
+    shape.style.borderLeft = `${this.size / 2}px solid transparent`;
+    shape.style.borderRight = `${this.size / 2}px solid transparent`;
+    shape.style.borderBottom = `${this.size}px solid ${this.color}`;
+    shape.style.position = "relative";
+    const textContainer = document.createElement("span");
+    textContainer.innerText = `€${this.price}`;
+    textContainer.style.position = "absolute";
+    textContainer.style.bottom = `${-(this.size * (6 / 9))}px`;
+    const pixel = this.size / 8;
+    textContainer.style.left = `${-pixel + pixel / 2}px`;
+    shape.appendChild(textContainer);
+    shape.setAttribute("id", `${this.shape} ${this.color} ${this.price}`);
+    return shape;
+  }
+  chooseShape() {
+    const shape = this.shape;
+    switch (shape) {
+      case "square":
+        return this.square();
+      case "circle":
+        return this.circle();
+      case "triangle":
+        return this.triangle();
+      default:
+        return;
+    }
+  }
+  get shapeColor() {
+    return this.color;
+  }
+  get shapePrice() {
+    return this.price;
+  }
+  set shapeColor(color) {
+    return (this.color = color);
+  }
+  set shapePrice(price) {
+    return (this.price = price);
+  }
+}
+// filter shapes
+class FilterObjects {
+  constructor(arr) {
+    this.arr = arr;
+    this.filteredShapes = [];
+  }
+  filterColor(objcolor) {
+    const filteredShapes = this.arr.filter((shape) =>
+      shape.id.includes(objcolor)
+    );
+    return filteredShapes;
+  }
+  filterShape(objshape) {
+    const filteredShapes = this.arr.filter((shape) =>
+      shape.id.includes(objshape)
+    );
+    return filteredShapes;
+  }
+  filterPrice(objprice) {
+    const filteredShapes = this.arr.filter((shape) =>
+      shape.id.includes(objprice)
+    );
+    return filteredShapes;
+  }
+  get filterNone() {
+    const filteredShapes = this.arr;
+    return filteredShapes;
+  }
+}
+// show shapes
+class ShowObjects {
+  constructor(shapesArray, attachTo) {
+    this.shapesArray = shapesArray;
+    this.attachTo = attachTo;
+  }
+  objectsGrid() {
+    this.shapesArray.forEach((shape) => {
+      const gridCell = document.createElement("div");
+      gridCell.style.width = "200px";
+      gridCell.append(shape);
+      this.attachTo.appendChild(gridCell);
+    });
+  }
+}
+//
+// grid cells
+const mySquare = new Shapes("square", 200);
+const myCircle = new Shapes("circle", 200);
+const myTriangle = new Shapes("triangle", 200);
+
+mySquare.shapeColor = "purple";
+mySquare.shapePrice = 0;
+myShapes.push(mySquare.chooseShape());
+
+myCircle.shapeColor = "purple";
+myCircle.shapePrice = "20";
+myShapes.push(myCircle.chooseShape());
+
+mySquare.shapeColor = "red";
+mySquare.shapePrice = "40";
+myShapes.push(mySquare.chooseShape());
+
+myCircle.shapeColor = "red";
+myCircle.shapePrice = "0";
+myShapes.push(myCircle.chooseShape());
+
+mySquare.shapeColor = "blue";
+mySquare.shapePrice = "20";
+myShapes.push(mySquare.chooseShape());
+
+myCircle.shapeColor = "blue";
+myCircle.shapePrice = "40";
+myShapes.push(myCircle.chooseShape());
+
+mySquare.shapeColor = "purple";
+mySquare.shapePrice = "60";
+myShapes.push(mySquare.chooseShape());
+
+myCircle.shapeColor = "purple";
+myCircle.shapePrice = "80";
+myShapes.push(myCircle.chooseShape());
+
+mySquare.shapeColor = "red";
+mySquare.shapePrice = "100";
+myShapes.push(mySquare.chooseShape());
+
+myCircle.shapeColor = "red";
+myCircle.shapePrice = "60";
+myShapes.push(myCircle.chooseShape());
+
+mySquare.shapeColor = "blue";
+mySquare.shapePrice = "80";
+myShapes.push(mySquare.chooseShape());
+
+myCircle.shapeColor = "blue";
+myCircle.shapePrice = "100";
+myShapes.push(myCircle.chooseShape());
+// triangle
+myTriangle.shapeColor = "darkgrey";
+myTriangle.shapePrice = "0";
+myShapes.push(myTriangle.chooseShape());
+
+myTriangle.shapeColor = "yellow";
+myTriangle.shapePrice = "33";
+myShapes.push(myTriangle.chooseShape());
+
+myTriangle.shapeColor = "orange";
+myTriangle.shapePrice = "66";
+myShapes.push(myTriangle.chooseShape());
+
+myTriangle.shapeColor = "brown";
+myTriangle.shapePrice = "99";
+myShapes.push(myTriangle.chooseShape());
+// \\ // \\ // \\
+const toBeFiltered = new FilterObjects(myShapes);
+let resultSet = toBeFiltered.filterNone;
+///////////////////////////////////////////////////////////////////////////////////
+
+// filter criteria
 // color \\
 const colorRed = document.querySelector("#red");
 const colorPurple = document.querySelector("#purple");
 const colorBlue = document.querySelector("#blue");
 // shape \\
 const shapeCircle = document.querySelector("#circle");
-const shapeRectangle = document.querySelector("#square");
+const shapeSquare = document.querySelector("#square");
 const shapeTriangle = document.querySelector("#triangle");
 // price \\
 const minPrice = document.querySelector("#min_price");
 const maxPrice = document.querySelector("#max_price");
 const showMinPrice = document.querySelector("#showMinPrice");
 const showMaxPrice = document.querySelector("#showMaxPrice");
+
 // button \\
 const resetValues = document.querySelector("#reset_button");
+
 // color eventlisteners \\
 colorRed.addEventListener("click", () => {
-    lprint(colorRed.checked == true ? "red checked!" : "red unchecked!");
+  if (colorRed.checked === true) {
+    resultSet = toBeFiltered.filterColor("red");
+    const myResult = new ShowObjects(resultSet, showShapes);
+    myResult.objectsGrid();
+  } else {
+    resultSet = toBeFiltered.filterNone;
+    const myResult = new ShowObjects(resultSet, showShapes);
+    myResult.objectsGrid();
+  }
 });
 colorPurple.addEventListener("click", () => {
-    lprint(colorPurple.checked == true ? "purple checked!" : "purple unchecked!");
+  lprint(colorPurple.checked ? "purple checked!" : "purple unchecked!");
 });
 colorBlue.addEventListener("click", () => {
-    lprint(colorBlue.checked == true ? "blue checked!" : "blue unchecked!");
+  lprint(colorBlue.checked ? "blue checked!" : "blue unchecked!");
 });
 // shape eventlisteners \\
 shapeCircle.addEventListener("click", () => {
-    lprint(shapeCircle.checked == true ? "circle checked!" : "circle unchecked!");
+  lprint(shapeCircle.checked ? "circle checked!" : "circle unchecked!");
 });
-shapeRectangle.addEventListener("click", () => {
-    lprint(shapeRectangle.checked == true ? "square checked!" : "square unchecked!");
+shapeSquare.addEventListener("click", () => {
+  lprint(shapeSquare.checked ? "square checked!" : "square unchecked!");
 });
 shapeTriangle.addEventListener("click", () => {
-    lprint(shapeTriangle.checked == true ? "triangle checked!" : "triangle unchecked!");
+  lprint(shapeTriangle.checked ? "triangle checked!" : "triangle unchecked!");
 });
 // price eventlisteners \\
 minPrice.addEventListener("input", () => {
-    showMinPrice.innerText = minPrice.value;
+  showMinPrice.innerText = minPrice.value;
 });
 maxPrice.addEventListener("input", () => {
-    showMaxPrice.innerText = maxPrice.value;
+  showMaxPrice.innerText = maxPrice.value;
 });
 // button eventlistener \\
 resetValues.addEventListener("click", () => {
-    colorRed.checked = false;
-    colorPurple.checked = false;
-    colorBlue.checked = false;
-    shapeCircle.checked = false;
-    shapeRectangle.checked = false;
-    shapeTriangle.checked = false;
-    showMinPrice.innerText = "0";
-    minPrice.value = 0;
-    showMaxPrice.innerText = "100";
-    maxPrice.value = 100;
-});
-////////////////////////////////////////////////////
-/*
-  SHAPES OBJECTS
-
-  One class with 3 shapes inside. Constructor parameters shape, size, color, price.
-*/
-////////////////////////////////////////////////////
-const showShapes = document.querySelector("#shapes");
-class Shapes {
-    constructor(shape, size, color, price) {
-        this._shape = shape;
-        this._size = size;
-        this._color = color;
-        this._price = price;
-    }
-    _circle() {
-        const shape = document.createElement("div");
-        shape.style.width = `${this._size}px`;
-        shape.style.height = `${this._size}px`;
-        shape.style.backgroundColor = this._color;
-        shape.style.borderRadius = "50%";
-        shape.style.display = "flex";
-        shape.style.justifyContent = "space-around";
-        shape.style.alignItems = "center";
-        shape.innerText = `€${this._price}`;
-        return shape;
-    }
-    _square() {
-        const shape = document.createElement("div");
-        shape.style.width = `${this._size}px`;
-        shape.style.height = `${this._size}px`;
-        shape.style.backgroundColor = this._color;
-        shape.style.display = "flex";
-        shape.style.justifyContent = "space-around";
-        shape.style.alignItems = "center";
-        shape.innerText = `€${this._price}`;
-        return shape;
-    }
-    _triangle() {
-        const shape = document.createElement("div");
-        shape.style.width = `${0}px`;
-        shape.style.height = `${0}px`;
-        shape.style.borderLeft = `${this._size / 2}px solid transparent`;
-        shape.style.borderRight = `${this._size / 2}px solid transparent`;
-        shape.style.borderBottom = `${this._size}px solid ${this._color}`;
-        shape.style.position = "relative";
-        const textContainer = document.createElement("span");
-        textContainer.innerText = `€${this._price}`;
-        textContainer.style.position = "absolute";
-        textContainer.style.bottom = `${-(this._size * (3 / 4))}px`;
-        const pixel = this._size / 8;
-        textContainer.style.left = `${-pixel + pixel / 2}px`;
-        shape.appendChild(textContainer);
-        return shape;
-    }
-    chooseShape() {
-        const shape = this._shape;
-        switch (shape) {
-            case "square":
-                return this._square();
-            case "circle":
-                return this._circle();
-            case "triangle":
-                return this._triangle();
-            default:
-                return;
-        }
-    }
-}
-
-const myShapes = [];
-
-const mySquare = new Shapes("square", 200, "red", 10);
-myShapes.push(mySquare.chooseShape());
-
-const myCircle = new Shapes("circle", 200, "purple", 20);
-myShapes.push(myCircle.chooseShape());
-
-const myTriangle = new Shapes("triangle", 200, "blue", 30);
-myShapes.push(myTriangle.chooseShape());
-
-myShapes.forEach((shape) => {
-    const gCell = document.createElement("div");
-    gCell.style.width = "200px";
-    gCell.style.outline = "1px dashed yellow";
-    gCell.append(shape);
-    showShapes.appendChild(gCell);
+  colorRed.checked = false;
+  colorPurple.checked = false;
+  colorBlue.checked = false;
+  shapeCircle.checked = false;
+  shapeRectangle.checked = false;
+  shapeTriangle.checked = false;
+  showMinPrice.innerText = "0";
+  minPrice.value = 0;
+  showMaxPrice.innerText = "100";
+  maxPrice.value = 100;
 });
--- filtering-by-shape-color-or-price/src/script.js
+++ filtering-by-shape-color-or-price/src/script.js
@@ -26,153 +26,276 @@ const dateTimestamp = () => {
     hour12: false,
     timeZone: "Europe/Amsterdam"
   };
-
   return new Intl.DateTimeFormat("nl-NL", options).format();
 };
 //============================
-// filters
+////////////////////////////////////////////////////
+/*
+  SHAPES OBJECTS
 
+  Classes: Shapes, FilterObjects, ShowObjects 
+*/
+////////////////////////////////////////////////////
+const showShapes = document.querySelector("#shapes");
+const myShapes = [];
+
+class Shapes {
+  constructor(shape, size) {
+    this.shape = shape;
+    this.size = size;
+    this.color = "white";
+    this.price = 0;
+  }
+  circle() {
+    const shape = document.createElement("div");
+    shape.style.width = `${this.size}px`;
+    shape.style.height = `${this.size}px`;
+    shape.style.backgroundColor = this.color;
+    shape.style.borderRadius = "50%";
+    shape.style.display = "flex";
+    shape.style.justifyContent = "space-around";
+    shape.style.alignItems = "center";
+    shape.innerText = `€${this.price}`;
+    shape.setAttribute("id", `${this.shape} ${this.color} ${this.price}`);
+    return shape;
+  }
+  square() {
+    const shape = document.createElement("div");
+    shape.style.width = `${this.size}px`;
+    shape.style.height = `${this.size}px`;
+    shape.style.backgroundColor = this.color;
+    shape.style.display = "flex";
+    shape.style.justifyContent = "space-around";
+    shape.style.alignItems = "center";
+    shape.innerText = `€${this.price}`;
+    shape.setAttribute("id", `${this.shape} ${this.color} ${this.price}`);
+    return shape;
+  }
+  triangle() {
+    const shape = document.createElement("div");
+    shape.style.width = `${0}px`;
+    shape.style.height = `${0}px`;
+    shape.style.borderLeft = `${this.size / 2}px solid transparent`;
+    shape.style.borderRight = `${this.size / 2}px solid transparent`;
+    shape.style.borderBottom = `${this.size}px solid ${this.color}`;
+    shape.style.position = "relative";
+    const textContainer = document.createElement("span");
+    textContainer.innerText = `€${this.price}`;
+    textContainer.style.position = "absolute";
+    textContainer.style.bottom = `${-(this.size * (6 / 9))}px`;
+    const pixel = this.size / 8;
+    textContainer.style.left = `${-pixel + pixel / 2}px`;
+    shape.appendChild(textContainer);
+    shape.setAttribute("id", `${this.shape} ${this.color} ${this.price}`);
+    return shape;
+  }
+  chooseShape() {
+    const shape = this.shape;
+    switch (shape) {
+      case "square":
+        return this.square();
+      case "circle":
+        return this.circle();
+      case "triangle":
+        return this.triangle();
+      default:
+        return;
+    }
+  }
+  get shapeColor() {
+    return this.color;
+  }
+  get shapePrice() {
+    return this.price;
+  }
+  set shapeColor(color) {
+    return (this.color = color);
+  }
+  set shapePrice(price) {
+    return (this.price = price);
+  }
+}
+// filter shapes
+class FilterObjects {
+  constructor(arr) {
+    this.arr = arr;
+    this.filteredShapes = [];
+  }
+  filterColor(objcolor) {
+    const filteredShapes = this.arr.filter((shape) =>
+      shape.id.includes(objcolor)
+    );
+    return filteredShapes;
+  }
+  filterShape(objshape) {
+    const filteredShapes = this.arr.filter((shape) =>
+      shape.id.includes(objshape)
+    );
+    return filteredShapes;
+  }
+  filterPrice(objprice) {
+    const filteredShapes = this.arr.filter((shape) =>
+      shape.id.includes(objprice)
+    );
+    return filteredShapes;
+  }
+  get filterNone() {
+    const filteredShapes = this.arr;
+    return filteredShapes;
+  }
+}
+// show shapes
+class ShowObjects {
+  constructor(shapesArray, attachTo) {
+    this.shapesArray = shapesArray;
+    this.attachTo = attachTo;
+  }
+  objectsGrid() {
+    this.shapesArray.forEach((shape) => {
+      const gridCell = document.createElement("div");
+      gridCell.style.width = "200px";
+      gridCell.append(shape);
+      this.attachTo.appendChild(gridCell);
+    });
+  }
+}
+//
+// grid cells
+const mySquare = new Shapes("square", 200);
+const myCircle = new Shapes("circle", 200);
+const myTriangle = new Shapes("triangle", 200);
+
+mySquare.shapeColor = "purple";
+mySquare.shapePrice = 0;
+myShapes.push(mySquare.chooseShape());
+
+myCircle.shapeColor = "purple";
+myCircle.shapePrice = "20";
+myShapes.push(myCircle.chooseShape());
+
+mySquare.shapeColor = "red";
+mySquare.shapePrice = "40";
+myShapes.push(mySquare.chooseShape());
+
+myCircle.shapeColor = "red";
+myCircle.shapePrice = "0";
+myShapes.push(myCircle.chooseShape());
+
+mySquare.shapeColor = "blue";
+mySquare.shapePrice = "20";
+myShapes.push(mySquare.chooseShape());
+
+myCircle.shapeColor = "blue";
+myCircle.shapePrice = "40";
+myShapes.push(myCircle.chooseShape());
+
+mySquare.shapeColor = "purple";
+mySquare.shapePrice = "60";
+myShapes.push(mySquare.chooseShape());
+
+myCircle.shapeColor = "purple";
+myCircle.shapePrice = "80";
+myShapes.push(myCircle.chooseShape());
+
+mySquare.shapeColor = "red";
+mySquare.shapePrice = "100";
+myShapes.push(mySquare.chooseShape());
+
+myCircle.shapeColor = "red";
+myCircle.shapePrice = "60";
+myShapes.push(myCircle.chooseShape());
+
+mySquare.shapeColor = "blue";
+mySquare.shapePrice = "80";
+myShapes.push(mySquare.chooseShape());
+
+myCircle.shapeColor = "blue";
+myCircle.shapePrice = "100";
+myShapes.push(myCircle.chooseShape());
+// triangle
+myTriangle.shapeColor = "darkgrey";
+myTriangle.shapePrice = "0";
+myShapes.push(myTriangle.chooseShape());
+
+myTriangle.shapeColor = "yellow";
+myTriangle.shapePrice = "33";
+myShapes.push(myTriangle.chooseShape());
+
+myTriangle.shapeColor = "orange";
+myTriangle.shapePrice = "66";
+myShapes.push(myTriangle.chooseShape());
+
+myTriangle.shapeColor = "brown";
+myTriangle.shapePrice = "99";
+myShapes.push(myTriangle.chooseShape());
+// \\ // \\ // \\
+const toBeFiltered = new FilterObjects(myShapes);
+let resultSet = toBeFiltered.filterNone;
+///////////////////////////////////////////////////////////////////////////////////
+
+// filter criteria
 // color \\
 const colorRed = document.querySelector("#red");
 const colorPurple = document.querySelector("#purple");
 const colorBlue = document.querySelector("#blue");
 // shape \\
 const shapeCircle = document.querySelector("#circle");
-const shapeRectangle = document.querySelector("#square");
+const shapeSquare = document.querySelector("#square");
 const shapeTriangle = document.querySelector("#triangle");
 // price \\
 const minPrice = document.querySelector("#min_price");
 const maxPrice = document.querySelector("#max_price");
 const showMinPrice = document.querySelector("#showMinPrice");
 const showMaxPrice = document.querySelector("#showMaxPrice");
+
 // button \\
 const resetValues = document.querySelector("#reset_button");
 
 // color eventlisteners \\
 colorRed.addEventListener("click", () => {
-  lprint(colorRed.checked == true ? "red checked!" : "red unchecked!");
+  if (colorRed.checked === true) {
+    resultSet = toBeFiltered.filterColor("red");
+    const myResult = new ShowObjects(resultSet, showShapes);
+    myResult.objectsGrid();
+  } else {
+    resultSet = toBeFiltered.filterNone;
+    const myResult = new ShowObjects(resultSet, showShapes);
+    myResult.objectsGrid();
+  }
 });
-
 colorPurple.addEventListener("click", () => {
-  lprint(colorPurple.checked == true ? "purple checked!" : "purple unchecked!");
+  lprint(colorPurple.checked ? "purple checked!" : "purple unchecked!");
 });
-
 colorBlue.addEventListener("click", () => {
-  lprint(colorBlue.checked == true ? "blue checked!" : "blue unchecked!");
+  lprint(colorBlue.checked ? "blue checked!" : "blue unchecked!");
 });
-
 // shape eventlisteners \\
 shapeCircle.addEventListener("click", () => {
-  lprint(shapeCircle.checked == true ? "circle checked!" : "circle unchecked!");
+  lprint(shapeCircle.checked ? "circle checked!" : "circle unchecked!");
 });
-
-shapeRectangle.addEventListener("click", () => {
-  lprint(
-    shapeRectangle.checked == true ? "square checked!" : "square unchecked!"
-  );
+shapeSquare.addEventListener("click", () => {
+  lprint(shapeSquare.checked ? "square checked!" : "square unchecked!");
 });
-
 shapeTriangle.addEventListener("click", () => {
-  lprint(
-    shapeTriangle.checked == true ? "triangle checked!" : "triangle unchecked!"
-  );
+  lprint(shapeTriangle.checked ? "triangle checked!" : "triangle unchecked!");
 });
-
 // price eventlisteners \\
 minPrice.addEventListener("input", () => {
   showMinPrice.innerText = minPrice.value;
 });
-
 maxPrice.addEventListener("input", () => {
   showMaxPrice.innerText = maxPrice.value;
 });
-
 // button eventlistener \\
 resetValues.addEventListener("click", () => {
   colorRed.checked = false;
   colorPurple.checked = false;
   colorBlue.checked = false;
-
   shapeCircle.checked = false;
   shapeRectangle.checked = false;
   shapeTriangle.checked = false;
-
   showMinPrice.innerText = "0";
   minPrice.value = 0;
   showMaxPrice.innerText = "100";
   maxPrice.value = 100;
 });
-////////////////////////////////////////////////////
-// object shapes
-/*
-  Three classes with arguments color, price and size.
-*/
-const showShapes = document.querySelector("#shapes");
-
-class Shapes {
-  static #size = "200px";
-  constructor(_price) {
-    this.price = _price;
-    this.#size = 200;
-  }
-  drawCircle(color) {
-    const shape = document.createElement("div");
-    shape.style.width = this.#size;
-    shape.style.height = this.#size;
-    shape.style.backgroundColor = color;
-    shape.style.borderRadius = "50%";
-    shape.style.display = "flex";
-    shape.style.justifyContent = "space-around";
-    shape.style.alignItems = "center";
-
-    shape.innerText = `€${this.price}`;
-    return shape;
-  }
-  drawSquare(color) {
-    const shape = document.createElement("div");
-    shape.style.width = `${this.size}px`;
-    shape.style.height = `${this.size}px`;
-    shape.style.backgroundColor = color;
-    shape.style.display = "flex";
-    shape.style.justifyContent = "space-around";
-    shape.style.alignItems = "center";
-
-    shape.innerText = `€${this.price}`;
-    return shape;
-  }
-  drawTriangle(color) {
-    const shape = document.createElement("div");
-    shape.style.width = `${0}px`;
-    shape.style.height = `${0}px`;
-    shape.style.borderLeft = `${this.size / 2}px solid transparent`;
-    shape.style.borderRight = `${this.size / 2}px solid transparent`;
-    shape.style.borderBottom = `${this.size}px solid ${color}`;
-    shape.style.position = "relative";
-
-    const textContainer = document.createElement("span");
-    textContainer.innerText = `€${this.price}`;
-
-    textContainer.style.position = "absolute";
-    textContainer.style.bottom = `${-(this.size * (3 / 4))}px`;
-
-    const pixel = this.size / 8;
-    textContainer.style.left = `${-pixel + pixel / 2}px`;
-
-    shape.appendChild(textContainer);
-    return shape;
-  }
-}
-
-myShapes = [];
-myShapes.forEach((shape) => {
-  const gCell = document.createElement("div");
-  gCell.style.width = "200px";
-  gCell.style.outline = "1px dashed yellow";
-  gCell.append(shape.drawTriangle());
-  showShapes.appendChild(gCell);
-});
-
-const myFigures = new Shapes(200, 200);
-showShapes.append(myFigures.drawSquare("orange"));
-showShapes.append(myFigures.drawCircle("brown"));
-showShapes.append(myFigures.drawTriangle("yellow"));

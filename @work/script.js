4deb375f1a4e23199b7d510a3d09cabf432d41d1
TestUserOnlineNL <testuser.onlinenl@gmail.com>
2023-06-05 13:14:55 +0200

    added and changes code
    solved a bug with help

--- @work/script.js
+++ @work/script.js
@@ -1,3 +1,4 @@
+console.clear();
 /*
   Sortable Lists
 */
@@ -5,63 +6,60 @@
 const items = document.querySelectorAll(".item");
 let draggableItem = null;
 
-items.forEach(item => {
-    item.addEventListener("dragstart", dragStart);
-    item.addEventListener("dragend", dragEnd);
+items.forEach((item) => {
+  item.addEventListener("dragstart", dragStart);
+  item.addEventListener("dragend", dragEnd);
 });
 
-function dragStart(e) {
-    draggableItem = this;
-    setTimeout(() => {
-        draggableItem.classList.add("dragging");
-        list.style.border = "1px solid red";
-    }, 0);
+function dragStart() {
+  draggableItem = this;
+  setTimeout(() => {
+    this.classList.add("dragging");
+  }, 0);
 }
 
-function dragEnd(e) {
-    setTimeout(() => {
-        draggableItem.classList.remove("dragging");
-        this.style.border = "1px solid black";
-        draggableItem = null;
-    }, 0);
+function dragEnd() {
+  draggableItem = null;
+  setTimeout(() => {
+    this.classList.remove("dragging");
+  }, 0);
 }
 
 // list
 const lists = document.querySelectorAll(".list");
 
 lists.forEach((list) => {
-    list.addEventListener("dragleave", dragLeave);
-    list.addEventListener("drop", dragDrop);
-    list.addEventListener("dragenter", dragEnter);
-    list.addEventListener("dragover", dragOver);
+  list.addEventListener("dragenter", dragEnter);
+  list.addEventListener("dragleave", dragLeave);
+  list.addEventListener("dragover", dragOver);
+  list.addEventListener("drop", dragDrop);
 
-    function dragDrop(e) {
-        e.preventDefault();
-        this.style.border = "1px solid black";
-        this.appendChild(draggableItem);
-    }
+  function dragOver(e) {
+    e.preventDefault();
+  }
 
-    function dragOver(e) {
+  function dragLeave() {
+    list.style.border = "1px solid black";
+  }
 
-        draggableItem = document.querySelector(".dragging");
+  function dragDrop(e) {
+    e.preventDefault();
+    let siblings = [...list.querySelectorAll(".item:not(.dragging)")];
 
-        // Getting all items except currently dragging and making an array of them
-        let siblings = [...list.querySelectorAll(".item:not(.dragging)")];
+    let nextSibling = siblings.find((sibling) => {
+      return e.pageY <= sibling.offsetTop + sibling.offsetHeight / 2;
+    });
 
-        // Finding the sibling after which the dragging item should be placed
-        let nextSibling = siblings.find(sibling => {
-            return e.clientY <= sibling.offsetTop + sibling.offsetHeight / 2;
-        });
+    list.insertBefore(draggableItem, nextSibling);
+    list.style.border = "1px solid black";
+  }
 
-        // Inserting the dragging item before the found sibling
-        list.insertBefore(draggableItem, nextSibling);
-    }
+  function dragEnter() {
+    list.style.border = "2px solid red";
+  }
 });
-
-function dragEnter(e) {
-    this.style.border = "1px solid red";
-}
-
-function dragLeave() {
-    this.style.border = "1px solid black";
-}
+//
+//=== kladblok ===
+//
+// https://stackoverflow.com/questions/44415228/list-sorting-with-html5-dragndrop-drop-above-or-below-depending-on-mouse
+//

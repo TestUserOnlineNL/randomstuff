5d5835f1ed21cdf1992a97bb6163a10d7d20072c
TestUserOnlineNL <testuser.onlinenl@gmail.com>
2023-08-24 15:52:37 +0200

    moved file and deleted one

--- lemp/php-files/index.php
+++ lemp/php-files/index.php
@@ -27,6 +27,14 @@
   echo "<br>";
   $myCar = new Car("red", "Toyota");
   echo $myCar->message();
+
+  #  ====
+
+  $age = array("Peter" => "35", "Ben" => "37", "Joe" => "43");
+
+  foreach ($age as $x => $val) {
+    echo "$x = $val<br>";
+  }
   ?>
 </body>
 

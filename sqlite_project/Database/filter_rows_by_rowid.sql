f2c505daa94191b88f21fe071504a218cf17c1d8
TestUserOnlineNL <testuser.onlinenl@gmail.com>
2023-09-04 14:13:58 +0200

    sql script changes

--- sqlite_project/Database/filter_rows_by_rowid.sql
+++ sqlite_project/Database/filter_rows_by_rowid.sql
@@ -1,12 +1,14 @@
 select
-	*
+	TITLE MOVIE_TITLE,
+	substring(YEAR,2,4) RELEASE_YEAR
 from
 	converted_imported_data
 where
 	ROWID = 1
 UNION ALL
 select
-	*
+	TITLE,
+	substring(YEAR,2,4)
 from
 	converted_imported_data
 where
@@ -19,7 +21,8 @@ where
 		converted_imported_data))
 UNION ALL
 select
-	*
+	TITLE,
+	substring(YEAR,2,4)
 from
 	converted_imported_data
 where

54096e70a1d3afa4e5edc07a6a92769fb9715cfa
TestUserOnlineNL <testuser.onlinenl@gmail.com>
2024-03-20 16:45:22 +0100

    more python3 code study

--- @work/series_regex.py
+++ @work/series_regex.py
@@ -1,12 +1,17 @@
 import re
 
-aString = "eenie meenie season 1 - 10 episodes watched"
+aString = "Black Lightning 3 - 16 episodes watched"
 
+def searchText(lineText):
 # get serie title, season number and episodes number and return them in a tuple
-#
-seriesData = re.search("^(.+)\sseason\s([0-9]+)\s\-\s([0-9]+)\s.+$", aString)
+    seriesData = re.search("^(.+)\s(season|deel)\s([0-9]+)\s\-\s([0-9]+)\s.+$", aString)
 
-if seriesData is None:
-    print(None)
-else:
-   print(seriesData.group(1,2,3))
+    if seriesData is None:
+        pass
+    else:
+        return(seriesData.group(1,2,3))
+
+    
+
+result = searchText(aString)
+print('result ',result)
